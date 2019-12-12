e05715bf7165c8609bc046e0342570c052904023
Alena Gilevskaya <alena@gilevskaya.com>
2019-12-12 03:06:07 -0800

    + order book

---
+++ web-sockets/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { OrderBook } from "./OrderBook";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <OrderBook />
+    </div>
+  );
+}
+
+export default App;
---
+++ web-sockets/src/OrderBook.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+export const OrderBook = () => {
+  const [bids, setBids] = React.useState();
+  const [asks, setAsks] = React.useState();
+  const [totalBids, setTotalBids] = React.useState();
+  const [totalAsks, setTotalAsks] = React.useState();
+
+  React.useEffect(() => {
+    const socket = new WebSocket(
+      "wss://www.bitmex.com/realtime?subscribe=orderBook10:XBTUSD"
+    );
+    socket.addEventListener("message", event => {
+      const data = JSON.parse(event.data).data;
+      if (Array.isArray(data) && data.length > 0) {
+        const { bids, asks } = data[0];
+        setBids(bids);
+        setAsks(asks);
+
+        setTotalBids(bids.reduce((acc, e) => acc + e[1], 0));
+        setTotalAsks(asks.reduce((acc, e) => acc + e[1], 0));
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "100px 100px 100px"
+        }}
+      >
+        {asks &&
+          asks.map((a, i) => (
+            <>
+              <div style={{ gridRow: asks.length - i }}>{a[0]}</div>
+              <div>{a[1]}</div>
+              <div>{((a[1] / totalAsks) * 100).toFixed(1)}</div>
+            </>
+          ))}
+      </div>
+      <div>--------------------</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "100px 100px 100px"
+        }}
+      >
+        {bids &&
+          bids.map(b => (
+            <>
+              <div>{b[0]}</div>
+              <div>{b[1]}</div>
+              <div>{((b[1] / totalBids) * 100).toFixed(1)}</div>
+            </>
+          ))}
+      </div>
+    </>
+  );
+};
