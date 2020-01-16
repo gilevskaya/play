00972e7700d9061ef9cfbab723a59260b782718f
Alena Gilevskaya <alena.gilevskaya@bitmex.com>
2020-01-16 13:34:19 -0800

    + icons

--- icons/src/App.js
+++ icons/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { ReactComponent as Icon } from "./myicon.svg";
 
 import "./App.css";
+import "./icons.svg";
 
 function App() {
   return (
@@ -10,6 +11,31 @@ function App() {
       <header className="App-header">
         <div style={{ border: "1px dashed white" }}>
           <Icon fill="salmon" width="10rem" />
+        </div>
+        <div>
+          <div style={{ marginTop: "1rem" }}>
+            <svg
+              width="10rem"
+              fill="white"
+              style={{ border: "1px dashed pink" }}
+            >
+              <use href="./icons.svg#bike" />
+            </svg>
+            <svg
+              width="10rem"
+              fill="salmon"
+              style={{ border: "1px dashed pink" }}
+            >
+              <use href="./icons.svg#bell" />
+            </svg>
+            <svg
+              width="10rem"
+              fill="#55ACEE"
+              style={{ border: "1px dashed pink" }}
+            >
+              <use href="./icons.svg#arrow" />
+            </svg>
+          </div>
         </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
